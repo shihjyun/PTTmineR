77c5ca64c6b33611a113c37714dc864c647fe4dd
shihjyun <shihjyun.yeo@gmail.com>
2019-12-02 01:27:12 +0800

    add future option

---
+++ R/future-option.R
@@ -0,0 +1,33 @@
+#' future option
+#'
+#' See \code{future::\link[future:plan]{plan}} for details.
+#'
+#' @name plan
+#' @rdname plan
+#' @keywords internal
+#' @export
+#' @importFrom future plan
+NULL
+#'
+#' future option
+#'
+#' See \code{future::\link[future:multiprocess]{multiprocess}} for details.
+#'
+#' @name multiprocess
+#' @rdname plan
+#' @keywords internal
+#' @export
+#' @importFrom future multiprocess
+#'
+NULL
+#' future option
+#'
+#' See \code{future::\link[future:sequential]{sequential}} for details.
+#'
+#' @name sequential
+#' @rdname plan
+#' @keywords internal
+#' @export
+#' @importFrom future sequential
+#'
+NULL
